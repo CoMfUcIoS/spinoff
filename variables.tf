4a0559f84ab02657065cf6c1ebdd690d3aa34982
Ioannis Karasavvaidis <32846251+CoMfUcIoS@users.noreply.github.com>
2025-01-12 12:50:05 +0000

    refactor: improve SSH key handling and cleanup configs (#27)
    - Commented out unused DockerHub username and tag in docker-image.yml
    - Removed unnecessary dashes in cron-connector.yml
    - Refactored main.tf to check for existing SSH keys and prevent destroy
    - Updated outputs.tf to use non-sensitive password
    - Added ssh_key_name and ssh_key_file variables in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,41 @@
-variable "hcloud_token" {
-  default     = ""
-  description = "Hetzner Cloud API token"
-}
-
-variable "faasd_node_name" {
-  default     = "faasd-node"
-  description = "Name of the faasd_node node"
-}
-
-variable "faasd_node_server_type" {
-  default     = "cx22"
-  description = "Server type of the faasd_node node"
-}
-
-variable "faasd_node_location" {
-  default     = "nbg1"
-  description = "Location of the faasd_node node"
+variable "ssh_key_name" {
+  description = "The name of the SSH key"
+  type        = string
+  default     = "faas-node-ssh-key"
 }
 
 variable "ssh_key_file" {
-  default     = "~/.ssh/id_terraform_rsa.pub"
   description = "Path to the SSH public key file"
+  type        = string
+  default     = "~/.ssh/id_terraform_rsa.pub"
 }
 
 variable "docker_user" {
   default     = "comfucios"
-  description = "DockerHub username to push the images"
+  description = "Docker username"
+  type        = string
+}
+
+variable "hcloud_token" {
+  description = "Hetzner Cloud API token"
+  type        = string
+  sensitive   = true
+}
+
+variable "faasd_node_name" {
+  default     = "faasd-node"
+  description = "Name of the faasd node"
+  type        = string
+}
+
+variable "faasd_node_server_type" {
+  default     = "cx22"
+  description = "Server type for the faasd node"
+  type        = string
+}
+
+variable "faasd_node_location" {
+  default     = "nbg1"
+  description = "Location for the faasd node"
+  type        = string
 }
